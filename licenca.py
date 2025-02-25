eac12e121f5f75f16ffb881a5e10e8c48695f18a
Luca Eleto <lucavilarino321@gmail.com>
2025-02-25 08:57:58 -0300

    Update licenca.py

--- licenca.py
+++ licenca.py
@@ -16,8 +16,8 @@ if st.button('Pesquisar'):
     cursor_consulta = con_origem.cursor()
     cursor_consulta.execute(consulta_sql, (f'%{st.consulta_sql}%',))
     cliente_consulta = cursor_consulta.fetchall()
-    tb = st.data_editor(cliente_consulta, columns=[ 'Cliente', 'Fantasia', 'dias'])
+    tb = pd.DataFrame(cliente_consulta, columns=[ 'Cliente', 'Fantasia', 'dias'])
     st.markdown('''
         ## Consulta Cliente
     ''')
-    st.table(tb)
+    st.data_editor(tb)
